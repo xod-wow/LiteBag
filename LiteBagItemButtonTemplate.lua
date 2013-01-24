64c4b770c834644e14c73a22ff59ae0e4fef6f9b
Mike "Xodiv" Battersby <mib@post.com>
2013-01-24 07:24:57 +0000

    Initial files.

---
+++ LiteBagItemButtonTemplate.lua
@@ -0,0 +1,148 @@
+--[[------------------------------------------------------------------------------
+
+  LiteBag/LiteBagItemButtonTemplate.lua
+
+  Copyright 2013 Mike Battersby
+
+------------------------------------------------------------------------------]]--
+
+function LiteBagItemButton_UpdateItem(self)
+
+    local bag = self:GetParent():GetID()
+    local slot = self:GetID()
+
+    local texture, count, _, _, readable = GetContainerItemInfo(bag, slot)
+
+    SetItemButtonTexture(self, texture)
+    SetItemButtonCount(self, count)
+
+    self.readable = readable
+
+    if texture then
+        self.hasItem = 1
+    else
+        self.hasItem = nil
+    end
+
+    if self == tooltipOwner then
+        LiteBagItemButton_UpdateTooltip(self)
+    end
+
+end
+
+function LiteBagItemButton_UpdateLocked(self)
+    local bag = self:GetParent():GetID()
+    local slot = self:GetID()
+
+    local locked = select(3, GetContainerItemInfo(bag, slot))
+    SetItemButtonDesaturated(self, locked)
+end
+
+function LiteBagItemButton_UpdateQuestTexture(self)
+    local bag = self:GetParent():GetID()
+    local slot = self:GetID()
+
+    local isQuestItem, questId, isActive = GetContainerItemQuestInfo(bag, slot)
+
+    if questId and not isActive then
+        self.questTexture:SetTexture(TEXTURE_ITEM_QUEST_BANG)
+        self.questTexture:Show()
+    elseif questId or isQuestItem then
+        self.questTexture:SetTexture(TEXTURE_ITEM_QUEST_BORDER)
+        self.questTexture:Show()
+    else
+        self.questTexture:Hide()
+    end
+end
+
+function LiteBagItemButton_UpdateCooldown(self)
+    local bag = self:GetParent():GetID()
+
+    if self.hasItem then
+        ContainerFrame_UpdateCooldown(bag, self)
+    else
+        _G[self:GetName() .. "Cooldown"]:Hide()
+    end
+end
+
+function LiteBagItemButton_UpdateFiltered(self)
+    local bag = self:GetParent():GetID()
+    local slot = self:GetID()
+
+    local quality, _, _, _, isFiltered = select(4, GetContainerItemInfo(bag, slot))
+
+    if isFiltered then
+        self.qualityTexture:Hide()
+        self.searchOverlay:Show()
+    else
+        if GetContainerItemQuestInfo(bag, slot) then
+            self.qualityTexture:Hide()
+        else
+            local r, g, b = GetItemQualityColor(quality)
+            self.qualityTexture:SetVertexColor(r, g, b)
+            self.qualityTexture:Show()
+        end
+        self.searchOverlay:Hide()
+    end
+end
+
+function LiteBagItemButton_Update(self)
+    LiteBagItemButton_UpdateItem(self)
+    LiteBagItemButton_UpdateQuestTexture(self)
+    LiteBagItemButton_UpdateLocked(self)
+    LiteBagItemButton_UpdateCooldown(self)
+    LiteBagItemButton_UpdateFiltered(self)
+end
+
+
+function LiteBagItemButton_OnLoad(self)
+    -- ContainerFrameItemButtonTemplate:GetScript("OnLoad")(self)
+    ContainerFrameItemButton_OnLoad(self)
+end
+
+function LiteBagItemButton_OnClick(self)
+    -- ContainerFrameItemButtonTemplate:GetScript("OnClick")(self)
+
+    -- This buggering around with IsModifiedClick taken from the OnClick
+    -- handler for ContainerFrameItemButtonTemplate.  See ContainerFrame.xml.
+
+    local treatAsModified = IsModifiedClick()
+    if button ~= "LeftButton" and treatAsModified and IsModifiedClick("AUTOLOOTTOGGLE") then
+        local bag = self:GetParent():GetID()
+        local slot = self:GetID()
+        local lootable = select(6, GetContainerItemInfo(bag, slot))
+        if lootable then
+            treatAsModified = false
+        end
+    end
+
+    if treatAsModified then
+        ContainerFrameItemButton_OnModifiedClick(self, button)
+    else
+        ContainerFrameItemButton_OnClick(self, button)
+    end
+end
+
+function LiteBagItemButton_OnEnter(self)
+    -- ContainerFrameItemButtonTemplate:GetScript("OnEnter")(self)
+    ContainerFrameItemButton_OnEnter(self)
+end
+
+function LiteBagItemButton_OnLeave(self)
+    -- ContainerFrameItemButtonTemplate:GetScript("OnLeave")(self)
+    Gametooltip:Hide()
+    ResetCursor()
+end
+
+function LiteBagItemButton_OnHide(self)
+    -- ContainerFrameItemButtonTemplate:GetScript("OnHide")(self)
+    if self.hasStackSplit and self.hasStackSplit == 1 then
+        StackSplitFrame:Hide()
+    end
+end
+
+function LiteBagItemButton_OnDrag(self)
+    -- ContainerFrameItemButtonTemplate:GetScript("OnDrag")(self)
+    ContainerFrameItemButton_OnDrag(self)
+end
+
---
+++ LiteBagTemplate.lua
@@ -0,0 +1,194 @@
+--[[------------------------------------------------------------------------------
+
+  LiteBag/LiteBagTemplate.lua
+
+  Copyright 2013 Mike Battersby
+
+------------------------------------------------------------------------------]]--
+
+function LiteBag_IsMyBag(self, id)
+    for _,bag in ipairs(self.bagIDs) do
+        if id == bag then return true end
+    end
+end
+
+function LiteBag_OnLoad(self)
+
+    if not self.bagIDs then
+        -- Error!  Needs self.bagIDs set before calling!
+        --  <Frame ... inherits="LiteBagTemplate">
+        --      <Scripts>
+        --          <OnLoad>
+        --              self.bagIDs = { 0, 1, 2, 3 }
+        --              LiteBag_OnLoad(self)
+        return
+    end
+
+    self.dummyContainerFrames = { }
+    self.itemButtons = { }
+
+    for _,bag in ipairs(self.BagIDs) do
+        self.dummyContainerFrames[bag] = CreateFrame("Frame", self:GetName() .. "ContainerFrame" .. bag, self)
+        self.dummyContainerFrames[bag]:SetID(bag)
+    end
+
+    SetBagPortraitTexture(self.portraitFrame, self.bagIDs[0])
+
+    self:RegisterEvent("BAG_OPEN")
+    self:RegisterEvent("BAG_CLOSED")
+    self:RegisterEvent("QUEST_ACCEPTED")
+    self:RegisterEvent("UNIT_QUEST_LOG_CHANGED")
+end
+
+function LiteBag_OnEvent(self, event, ...)
+    if event == "BAG_OPEN" then
+        local bag = ...
+        if LiteBag_IsMyBag(self, bag) then
+            self:Show()
+        end
+    elseif event == "BAG_CLOSED" then
+        local bag = ...
+        if LiteBag_IsMyBag(self, bag) then
+            self:Hide()
+        end
+    elseif event == "BAG_UPDATE" then
+        local bag = ...
+        if LiteBag_IsMyBag(self, bag) then
+            LiteBag_Update(self)
+        end
+    elseif event == "ITEM_LOCK_CHANGED" then
+        local bag, slot = ...
+        if bag and slot and LiteBag_IsMyBag(self, bag) then
+            LiteBag_UpdateLocked(self)
+        end
+    elseif event == "BAG_UPDATE_COOLDOWN" then
+        local bag = ...
+        if LiteBag_IsMyBag(self, bag) then
+            LiteBag_UpdateCooldowns(self)
+        end
+    elseif event == "QUEST_ACCEPTED" or event == "UNIT_QUEST_LOG_CHANGED" then
+        LiteBag_UpdateQuestTextures(self)
+    elseif event == "INVENTORY_SEARCH_UPDATE" then
+        LiteBag_UpdateSearchResults(self)
+    elseif event == "DISPLAY_SIZE_CHANGED" then
+        LiteBag_UpdateLayout(self)
+    end
+end
+
+function LiteBag_SetMainMenuBarButtons(self, checked)
+    if LiteBag_IsMyBag(BACKPACK_CONTAINER) then
+        MainMenuBarBackpackButton:SetChecked(checked)
+    end
+
+    for n = 1, NUM_CONTAINER_FRAMES do
+        if LiteBag_IsMyBag(n) then
+            local button = _G["CharacterBag"..(n-1).."Slot"]
+            if button then
+                button:SetChecked(checked)
+            end
+        end
+    end
+end
+
+function LiteBag_OnHide(self)
+    self:UnregisterEvent("BAG_UPDATE")
+    self:UnregisterEvent("ITEM_LOCK_CHANGED")
+    self:UnregisterEvent("BAG_UPDATE_COOLDOWN")
+    self:UnregisterEvent("DISPLAY_SIZE_CHANGED")
+    self:UnregisterEvent("INVENTORY_SEARCH_UPDATE")
+
+    LiteBag_SetMainMenuBarButtons(self, 0)
+
+    PlaySound("igBackPackClose")
+end
+
+function LiteBag_OnShow(self)
+    self:RegisterEvent("BAG_UPDATE")
+    self:RegisterEvent("ITEM_LOCK_CHANGED")
+    self:RegisterEvent("BAG_UPDATE_COOLDOWN")
+    self:RegisterEvent("DISPLAY_SIZE_CHANGED")
+    self:RegisterEvent("INVENTORY_SEARCH_UPDATE")
+
+    self:Update()
+
+    LiteBag_SetMainMenuBarButtons(self, 1)
+
+    PlaySound("igBackPackOpen")
+end
+
+function LiteBag_AttachSearchBox(self)
+    BagItemSearchBox:SetParent(self)
+    BagItemSearchBox:SetPoint("TOPRIGHT", self, "TOPRIGHT", -10, -26)
+    BagItemSearchBox.anchorBag = self
+    BagItemSearchBox:Show()
+end
+
+function LiteBag_UpdateCooldowns(self)
+    for i = 1, self.size do
+        LiteMountItemButton_UpdateCooldown(self.itemButtons[i])
+    end
+end
+
+function LiteBag_UpdateLocked()
+    for i = 1, self.size do
+        LiteMountItemButton_UpdateLocked(self.itemButtons[i])
+    end
+end
+
+function LiteBag_UpdateQuestTextures()
+    for i = 1, self.size do
+        LiteMountItemButton_UpdateQuestTexture(self.itemButtons[i])
+    end
+end
+
+function LiteBag_CreateItemButton(self, i)
+    local b = CreateFrame("Button", self:GetName().."Item"..i, self, "LiteBagItemButtonTemplate")
+    self.itemButtons[i] = b
+end
+
+function LiteBag_CreateItemButtons(self)
+    local n = 1
+
+    self.size = 0
+
+    for _,bag in ipairs(self.bagIDs) do
+        for slot = 0, GetNumContainerSlots(bag) do
+            if not self.itemButtons[n] then
+                LiteBag_CreateItemButton(self, n)
+            end
+            self.itemButtons[n]:SetID(slot)
+            self.itemButtons[n]:SetParent(self.dummyContainerFrames[bag])
+            self.size = self.size + 1
+            n = n + 1
+        end
+    end
+end
+
+function LiteBag_PositionItemButtons(self)
+    local name = self:GetName()
+
+    for i = 1, self.size do
+        local itemButton = self.itemButtons[i]
+        -- XXX Pack them on top of each other bottom right for now XXX
+        itemButton:SetPoint("BOTTOMRIGHT", name, "BOTTOMRIGHT", -12, 9 + self.moneyFrame:GetHeight())
+    end
+end
+
+function LiteBag_Update(self)
+
+    if not self:IsShown() then return end
+
+    LiteBag_AttachSearchBox(self)
+
+    LiteBag_CreateItemButtons(self)
+    LiteBag_PositionItemButtons(self)
+
+    for i,itemButton in ipairs(self.itemButtons) do
+        if i <= self.size then
+            itemButton:Update()
+            itemButton:Show()
+        else
+            itemButton:Hide()
+        end
+    end
+end
