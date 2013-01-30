55f7fe1362e18a49899a95b061abeffcf5e69c5c
Mike "Xodiv" Battersby <mib@post.com>
2013-01-30 04:03:03 +0000

    Start of adding bag buttons. Add developer notes file.

---
+++ LiteBagBagButtonTemplate.lua
@@ -0,0 +1,13 @@
+--[[----------------------------------------------------------------------------
+
+  LiteBag/LiteBagBagmButtonTemplate.lua
+
+  Copyright 2013 Mike Battersby
+
+----------------------------------------------------------------------------]]--
+
+function LiteBagBagButton_OnEnter(self)
+    -- TODO: Highlight the slots in the bag
+    BankFrameItemButton_OnEnter(self)
+end
+
