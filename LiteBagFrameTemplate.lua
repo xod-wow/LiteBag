182dc6d5bdeaf4a3b8c9179fdb2c2268066580a9
Mike "Xodiv" Battersby <mib@post.com>
2013-02-02 13:31:56 +0000

    Fix bank to update again.

--- LiteBagFrameTemplate.lua
+++ LiteBagFrameTemplate.lua
@@ -89,7 +89,7 @@ function LiteBagFrame_OnEvent(self, event, ...)
         end
     elseif event == "PLAYERBANKSLOTS_CHANGED" then
         local slot = ...
-        if self.isBank and slot > NUM_BANKGENERIC_SLOTS then
+        if self.isBank then
                 LiteBagFrame_Update(self)
         end
     elseif event == "ITEM_LOCK_CHANGED" then
