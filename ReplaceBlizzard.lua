5659d82f957cde769c15b04b5b54ac6e6edd0175
Mike "Xodiv" Battersby <mib@post.com>
2014-10-16 09:17:35 +0000

    Copyright years.

--- ReplaceBlizzard.lua
+++ ReplaceBlizzard.lua
@@ -2,7 +2,7 @@
 
   LiteBag/ReplaceBlizzard.lua
 
-  Copyright 2013 Mike Battersby
+  Copyright 2013-2014 Mike Battersby
 
   Released under the terms of the GNU General Public License version 2 (GPLv2).
   See the file LICENSE.txt.
