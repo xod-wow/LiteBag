d1cf6a212b180feb13d7637a749f80eec8cf2fe5
Mike "Xodiv" Battersby <mib@unimelb.edu.au>
2014-11-17 09:42:16 +1100

    Remove some errant spaces

--- LiteBagBagButtonTemplate.lua
+++ LiteBagBagButtonTemplate.lua
@@ -52,7 +52,7 @@ function LiteBagBagButton_Update(self)
     self.bagID = self:GetID()
     self.isBank = BankContainers[self:GetID()]
 
-    -- XXX FIXME XXX 
+    -- XXX FIXME XXX
     -- No slotID for backpack/bank, can they not be filtered?
 
     if self.bagID == BACKPACK_CONTAINER then
