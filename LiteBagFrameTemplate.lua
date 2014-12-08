995d15db5e6ee78128d09a41bb6e0d77327a0eea
Mike "Xodiv" Battersby <mib@post.com>
2014-12-08 15:57:45 +1100

    Leave the selectedTab setting to the hook.

--- LiteBagFrameTemplate.lua
+++ LiteBagFrameTemplate.lua
@@ -610,10 +610,6 @@ end
 function LiteBagFrame_TabOnClick(self)
     local parent = self:GetParent()
 
-    if self.isBank then
-        BankFrame.selectedTab = self:GetID()
-    end
-
     PanelTemplates_SetTab(parent, self:GetID())
     LiteBagFrame_Update(parent)
 
