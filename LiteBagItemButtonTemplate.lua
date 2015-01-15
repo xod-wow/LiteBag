6317648db80fae3d51de8efa99bcc7666d9c747c
Mike "Xodiv" Battersby <mib@post.com>
2015-01-15 19:39:30 +1100

    Updated comments on frame internals.

--- LiteBagItemButtonTemplate.lua
+++ LiteBagItemButtonTemplate.lua
@@ -12,37 +12,44 @@
 --[[----------------------------------------------------------------------------
   Frame components of a LiteBagItemButtonTemplate frame.
 
-  Inherited from ContainerFrameItemButtonTemplate:
-    $parentIconTexture (Texture) also frame.icon
+  Inherited from ItemButtonTemplate:
+    $parentIconTexture (Texture level=BORDER/0) also self.icon
         The main icon image for the item
-    $parentCount (FontString)
+    self.IconBorder (Texture level=OVERLAY/0)
+        An overlay for the button border that colors it according to the
+        quality of the item.
+    $parentCount (FontString level=ARTWORK/2)
         The stack count (a number) attached at the bottom right.
-    $parentStock (FontString)
-        The number in stock at the vendor.  It's unused in for the bag items
-        so we re-size it and re-font it and use it for the Equipment Set name.
-        Attached at the top left.
-    $parentSearchOverlay (Texture)
+    $parentStock (FontString level=ARTWORK/2)
+        The number in stock at the vendor.
+    self.searchOverlay (Texture level=OVERLAY/0)
         Shown when the item does not match a bag search.  It's a black texture
         with 0.8 alpha that covers the entire icon and blacks it out.
     $parentNormalTexture (Texture)
         This is the button outline stuff.  There's also corresponding Pushed
         and Highlight texture that aren't named.  We don't touch this, it's
         just part of the normal button click behaviour stuff.
-    $parentIconQuestTexture (Texture)
+
+  Inherited from ContainerFrameItemButtonTemplate:
+    $parentIconQuestTexture (Texture level=ARTWORK/1)
         Shows the gold ! on items that will start a quest if you click them.
-    $parentNewItemTexture (Texture)
-        Show something (not sure what yet) for items you just bought from the
-        in-game store.
+    self.JunkIcon (Texture level=OVERLAY/1)
+        Gold coin icon shown when a vendor is open for grey items.
+    self.flash (Texture level=OVERLAY/1)
+        Edge flash for new items, used by an associated Animation.
+    self.NewItemTexture (Texture level=OVERLAY/1)
+        Edge glow for "new" items whatever the API returns one is.
+    self.BattlePayItemTexture (Texture level=OVERLAY/1)
+        Highlight for imaginary items you paid good money for.
     $parentCooldown (Cooldown)
         Normal item cooldown frame (does the sweep etc.).
-    $parent.IconBorder
-        An overlay for the button border that colors it according to the
-        quality of the item.  Used to be LiteBag's qualityTexture prior to
-        Blizzard adding it in 6.0.2.
 
   Added by LiteBagItemButtonTemplate:
-    $parentBackgroundTexture (Texture) also frame.backgroundTexture
+    self.backgroundTexture (Texture level=BACKGROUND/0)
         The slot background, so that empty slots have a texture.
+    self.eqTexture1/2/3/4 (Texture level=ARTWORK/1)
+        Textures shown when the item is part of one of the first
+        four EquipmentSets.
 
 ----------------------------------------------------------------------------]]--
 
@@ -197,16 +204,15 @@ function  LiteBagItemButton_UpdateItemLevel(self)
     local bag = self:GetParent():GetID()
     local slot = self:GetID()
 
-    local stockText = _G[self:GetName() .. "Stock"]
-
+    -- XXX FIXME XXX self.itemLevel isn't in the XML yet.
     local _, _, _, quality, _, _, link, _ = GetContainerItemInfo(bag, slot)
     if link then
         local iLevel = select(4, GetItemInfo(link))
         if iLevel then
             local r, g, b = GetItemQualityColor(quality)
-            stockText:SetText(iLeveL)
-            stockText:Show()
-            stockText:SetTextColor(r,g,b)
+            self.itemLevel:SetText(iLevel)
+            self.itemLevel:Show()
+            self.itemLevel:SetTextColor(r,g,b)
             return
         end
     end
@@ -225,18 +231,6 @@ local function ContainerItemIsPartOfEquipmentSet(bag, slot, i)
     end
     return false
 
---  local _, equipsetNames = GetContainerItemEquipmentSetInfo(bag, slot)
---  if equipsetNames then
---      for _,name in ipairs({ strsplit(",", equipSetNames) })  do
---          local n = nameMap[name]
---      for i=1,3 do
---          local n = GetEquipmentSetInfo(i)
---      local icon, setID, isEquipped, numItems, numEquipped, _, numMissing, numIgnored = GetEquipmentSetInfoByName(equipstName)
---      stockText:SetText(gsub(equipsetName, ", ", "\n"))
---      stockText:Show()
---  else
---      stockText:Hide()
---  end
 end
 
 function LiteBagItemButton_UpdateEquipmentSets(self)
@@ -270,17 +264,6 @@ function LiteBagItemButton_OnLoad(self)
     ContainerFrameItemButton_OnLoad(self)
     self.GetInventorySlot = ButtonInventorySlot
     self.UpdateTooltip = LiteBagItemButton_OnEnter
-
-    -- We (mis)use the "number in stock at vendor" text to show the equipset.
-    -- By default it's only attached at TOPLEFT.  Attach it to BOTTOMRIGHT as
-    -- well so we get multiline and automatic truncation of the text.  And
-    -- set the font size a little smaller (by default it uses
-    -- NumberFontNormalYellow (see FontStyles.xml in the Blizzard UI source).
-
-    local stockText = _G[self:GetName() .. "Stock"]
-    stockText:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", 0, 2)
-    stockText:SetJustifyV("TOP")
-    stockText:SetFont("Fonts\\ARIALN.TTF", 12, "OUTLINE")
 end
 
 function LiteBagItemButton_OnEnter(self)
