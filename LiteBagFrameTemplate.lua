ce8101934c1fe9b5444120f00c637ee5fcd94227
Mike "Xodiv" Battersby <mib@post.com>
2016-08-14 22:06:10 +1000

    Fix bug with sizing/placement of sort button on bank. Sadly unable to be compatible with Leatrix Plus.

--- LiteBagFrameTemplate.lua
+++ LiteBagFrameTemplate.lua
@@ -428,11 +428,13 @@ end
 
 function LiteBagFrame_AttachSearchBox(self)
     self.searchBox:SetParent(self)
+    self.searchBox:ClearAllPoints()
     self.searchBox:SetPoint("TOPRIGHT", self, "TOPRIGHT", -38, -35)
     self.searchBox.anchorBag = self
     self.searchBox:Show()
 
     self.sortButton:SetParent(self)
+    self.sortButton:ClearAllPoints()
     self.sortButton:SetPoint("TOPRIGHT", self, "TOPRIGHT", -7, -32)
     self.sortButton.anchorBag = self
     self.sortButton:Show()
