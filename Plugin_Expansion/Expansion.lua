c40f5169c4dcdbc0b22cd8b674db2b7604b45b68
Mike "Xodiv" Battersby <mib@post.com>
2024-08-04 21:43:23 +1000

    Add a debug expansion annotator

---
+++ Plugin_Expansion/Expansion.lua
@@ -0,0 +1,41 @@
+--[[----------------------------------------------------------------------------
+
+  LiteBag/Plugin_Expansion/Expansion.lua
+
+  Copyright 2013 Mike Battersby
+
+  Released under the terms of the GNU General Public License version 2 (GPLv2).
+  See the file LICENSE.txt.
+
+----------------------------------------------------------------------------]]--
+
+local addonName, LB = ...
+
+local L = LB.Localize
+
+local TextsByButton = {}
+
+local function Update(button, bag, slot)
+    if TextsByButton[button] == nil then
+        TextsByButton[button] = button:CreateFontString(nil, "ARTWORK", "GameFontNormalOutline")
+        TextsByButton[button]:SetPoint("LEFT", button, "LEFT", 1, 0)
+    end
+
+    if LB.GetGlobalOption("showExpansion") then
+        local id = C_Container.GetContainerItemID(bag, slot)
+
+        if id then
+            local expansionID = select(15, C_Item.GetItemInfo(id))
+            if expansionID then
+                local name = GetExpansionName(expansionID)
+                TextsByButton[button]:SetText(expansionID)
+                TextsByButton[button]:Show()
+                return
+            end
+        end
+    end
+    TextsByButton[button]:Hide()
+end
+
+-- LiteBag
+LB.RegisterHook('LiteBagItemButton_Update', Update, true)
