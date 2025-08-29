6dabc3604aee1761eda62697f45708ab519e2da7
Mike "Xodiv" Battersby <mib@post.com>
2025-08-29 19:32:52 +1000

    Now with less taint. Maybe

--- ContainerFrame.lua
+++ ContainerFrame.lua
@@ -27,7 +27,7 @@ local BlizzardContainerFrames = {
 
 --------------------------------------------------------------------------------
 
-local mixin = {}
+local override = {}
 
 local function BagsOffsetFunction(row, col)
     local xoff, yoff = 0, 0
@@ -42,17 +42,43 @@ local function BagsOffsetFunction(row, col)
     return -xoff, yoff
 end
 
-function mixin:GetAnchorLayout()
-    local layout = ContainerFrameMixin.GetAnchorLayout(self)
-    layout:SetCustomOffsetFunction(BagsOffsetFunction)
-    return layout
+local function SortItemsByExtendedState(item1, item2)
+    local extended1, extended2 = item1:IsExtended(), item2:IsExtended()
+    if extended1 ~= extended2 then return not extended1 end
+
+    local bag1, bag2 = item1:GetBagID(), item2:GetBagID()
+    if bag1 ~= bag2 then return bag1 > bag2 end
+
+    local id1, id2 = item1:GetID(), item2:GetID()
+    return id1 < id2
 end
 
-function mixin:CalculateHeight()
-    local h = ContainerFrameCombinedBagsMixin.CalculateHeight(self)
+local function UpdateItemSort(items)
+    if not IsAccountSecured() then
+        table.sort(items, SortItemsByExtendedState)
+    end
+end
+
+function override:UpdateItemLayout()
+    local itemsToLayout = {}
+    for _, itemButton in self:EnumerateValidItems() do
+        table.insert(itemsToLayout, itemButton)
+    end
+    UpdateItemSort(itemsToLayout)
+    local columns = override.GetColumns(self)
+    local layout = AnchorUtil.CreateGridLayout(GridLayoutMixin.Direction.BottomRightToTopLeft, columns, ITEM_SPACING_X, ITEM_SPACING_Y)
+    layout:SetCustomOffsetFunction(BagsOffsetFunction)
+    AnchorUtil.GridLayout(itemsToLayout, self:GetInitialItemAnchor(), layout)
+end
+
+function override:CalculateHeight()
+    local rows = override.GetRows(self)
+    local templateInfo = C_XMLUtil.GetTemplateInfo(self.itemButtonPool:GetTemplate())
+    local itemsHeight = (rows * templateInfo.height) + ((rows - 1) * ITEM_SPACING_Y)
+    local h = itemsHeight + self:GetPaddingHeight() + self:CalculateExtraHeight()
     local yBreak = LB.GetTypeOption('BACKPACK', 'ybreak') or 0
     if yBreak > 0 then
-        local rows = self:GetRows()
+        local rows = override.GetRows(self)
         local gapHeight = math.floor((rows-1)/3)* ITEM_SPACING_X * 2
         return h + gapHeight
     else
@@ -60,11 +86,14 @@ function mixin:CalculateHeight()
     end
 end
 
-function mixin:CalculateWidth()
-    local w = ContainerFrameCombinedBagsMixin.CalculateWidth(self)
+function override:CalculateWidth()
+    local columns = override.GetColumns(self)
+    local templateInfo = C_XMLUtil.GetTemplateInfo(self.itemButtonPool:GetTemplate())
+    local itemsWidth = (columns * templateInfo.width) + ((columns - 1) * ITEM_SPACING_X)
+    local w = itemsWidth + self:GetPaddingWidth()
     local xBreak = LB.GetTypeOption('BACKPACK', 'xbreak') or 0
     if xBreak > 0 then
-        local columns = self:GetColumns()
+        local columns = override.GetColumns(self)
         local gapWidth = math.floor((columns-1)/xBreak) * ITEM_SPACING_Y * 2
         return w + gapWidth
     else
@@ -72,13 +101,24 @@ function mixin:CalculateWidth()
     end
 end
 
-function mixin:GetColumns()
+function override:UpdateFrameSize()
+    local width = override.CalculateWidth(self)
+    local height = override.CalculateHeight(self)
+    self:SetSize(width, height)
+end
+
+function override:GetColumns()
     local default = ContainerFrameMixin.GetColumns(self)
     local nCols = LB.GetTypeOption('BACKPACK', 'columns') or default
     return nCols
 end
 
-function mixin:SetSearchBoxPoint(searchBox)
+function override:GetRows()
+    return math.ceil(self:GetBagSize() / override.GetColumns(self))
+end
+
+function override:SetSearchBoxPoint(searchBox)
+    searchBox:ClearAllPoints()
     searchBox:SetPoint("RIGHT", BagItemAutoSortButton, "LEFT", -12, 0)
     searchBox:SetWidth(math.min(330, self:GetWidth() - 110))
 end
@@ -98,8 +138,12 @@ local function HookBlizzardBags()
     end
 end
 
+local hooks = { "UpdateFrameSize", "UpdateItemLayout", "SetSearchBoxPoint" }
+
 function LB.PatchBags()
-    Mixin(ContainerFrameCombinedBags, mixin)
+    for _, hook in ipairs(hooks) do
+        hooksecurefunc(ContainerFrameCombinedBags, hook, override[hook])
+    end
     HookBlizzardBags()
 end
 
